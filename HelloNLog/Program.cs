f5849024b2dd1d1b7c1dd010def2b3d926200fe7
Fabio Galante Mans <fgamans@hotmail.com>
2020-04-18 19:35:34 -0300

    FizzBuzz

---
+++ HelloNLog/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace HelloNLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NLogMethod();
+        }
+
+        private static void NLogMethod()
+        {
+            ConsoleTarget consoleTarget = new ConsoleTarget
+            {
+                Name = "consoleTarget",
+                Layout = "${message}"
+            };
+
+            LoggingConfiguration loggingConfiguration = new LoggingConfiguration();
+
+            loggingConfiguration.AddRuleForAllLevels(consoleTarget);
+
+            LogFactory logFactory = new LogFactory(loggingConfiguration);
+
+            Logger logger = logFactory.GetLogger("PluralsightLogger");
+
+            logger.Info("Teste");
+        }
+    }
+}
